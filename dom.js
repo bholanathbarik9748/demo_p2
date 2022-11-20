cccc5795fe17cbcf49f93dbe38248fe60af4fe21
Bholanath <bholanathbarik9748@gmail.com>
2022-11-20 22:32:30 +0530

    version 5

--- dom.js
+++ dom.js
@@ -33,13 +33,59 @@
 // }
 
 // Get Element by Tag name
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].style.backgroundColor = "yellow";
-li[1].textContent = "yo1";
-li[1].style.fontWight = "bold";
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].style.backgroundColor = "yellow";
+// li[1].textContent = "yo1";
+// li[1].style.fontWight = "bold";
 
-for(var i = 0;i < li.length;i++){
-    li[i].style.backgroundColor = '#f4f4f4';
+// for(var i = 0;i < li.length;i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
+
+// QuerySelector
+var head = document.querySelector('#main-header');
+head.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value = "Yo Bro";
+
+var sub = document.querySelector('input[type = "submit"]');
+sub.value = "Send";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = "red";
+
+var item1 = document.querySelector('.list-group-item:last-child');
+item1.style.color = "blue";
+
+
+var item2 = document.querySelector('.list-group-item:last-child');
+item2.style.color = "blue";
+
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.color = "green";
+
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
+item2.style.visibility = hidden;
+
+// QuerySelectorALL
+var ti = document.querySelectorAll('.title');
+ti[0].textContent = 'Yo';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i = 0;v < odd.length;i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
 }
+
+var odd = document.querySelectorAll('li:nth-child(even)');
+for(var i = 0;v < even.length;i++){
+    even[i].style.backgroundColor = '#ccc';
+}
+
+var item2 = document.querySelector('.list-group-item:nth-child(2)');
+item2.style.color = "green";
+
+var item3 = document.querySelector('.list-group-item:nth-child(3)');
+item2.style.visibility = hidden;
