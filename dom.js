7ec56d9d0a32906d84d2d235d7f989c4e7ed042a
Bholanath <bholanathbarik9748@gmail.com>
2022-11-20 20:04:01 +0530

    version1

---
+++ dom.js
@@ -0,0 +1,14 @@
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.title);
+// // document.title = "ABC";
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+
+// Get Element by iD
+var cnt = document.getElementById('header-title');
+console.log(cnt);
